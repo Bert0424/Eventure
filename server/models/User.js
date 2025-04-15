6f67f42a8796680e844a6364683c3ec39f4e53f1
t3nn-source <Jamesleel.2002@gmail.com>
2025-04-15 12:21:27 -0500

    Adding Event and User models for the database

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,45 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-  // add fields later
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match : [/.+@.+\..+/, 'Please fill a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  savedEvents: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event',
+    }],
+}, {
+  timestamps: true,
 });
 
-module.exports = mongoose.model('User', userSchema);
+// Hash password before saving to the database
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+// Compare passwords for authentication
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
