dc45c962d58f32ef94d62a9f4d2b5a6f28dff092
Albert D <bert0424@gmail.com>
2025-04-16 18:42:14 -0500

    Completed front end

---
+++ client/src/components/EventList.js
@@ -0,0 +1,15 @@
+import EventCard from "./EventCard";
+
+function EventList({ events }) {
+  return (
+    <div className="row fade-in">
+      {events.map((event) => (
+        <div key={event.id} className="col-md-4">
+          <EventCard event={event} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default EventList;
